94aec229fdc1dbe98bb8c3322b35ab368f4143bd
tutqq96 <tutqq96@gmail.com>
2023-10-23 23:28:34 +0700

    Initial commit

---
+++ config-server/src/test/java/com/uth/ums/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uth.ums.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service-registry/src/test/java/com/uth/ums/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uth.ums.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
